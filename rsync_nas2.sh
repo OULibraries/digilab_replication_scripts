baf7312b0701ec3cf36440deabf1dd53ef9f7d5f
MP <mpmalahy@ou.edu>
2024-08-02 16:15:06 -0500

    updated validate_bags to include functionality from verify_bags

--- rsync_nas2.sh
+++ rsync_nas2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Check for files that are 0kb and append to list for exclusion.
-/bin/bash/ verify_bags.sh $NAS2
+#Executes python script to validate bags and assigns output to variable
+VALID_BAGS=$(python3 validate_bags.py $NAS2)
 
-# calls rsync_to_norfile script and passes Logfile and Mount variable arguments for nas2
-/bin/bash/ rsync_to_norfile.sh  $INCLUDE_PRIVATE_ONLY_BAGIT2 $INCLUDE_PUBLIC_ONLY_BAGIT2
+# calls rsync_to_norfile script and passes validated bags to script
+/bin/bash/ rsync_to_norfile.sh  $VALID_BAGS
--- validate_bags.py
+++ validate_bags.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python
 
+import os
 import sys
 import bagit
 
 from config import *
 
-source_path = sys.argv[1]
+rootdir = sys.argv[1]
 
 valid_bags = []
-incomplete_bags = []
 
-bags = bagit.Bag(source_path)
-    for bag in bags:
-        if bag.is_valid():
-            valid_bags.append(source_path)
-        else:
-            incomplete_bags.append(source_path)
-
-##I need to assign valid bags to a variable and use that in the rsync.
+for subdir in os.walk(rootdir):
+    bags = bagit.Bag(dirname)
+        for bag in bags:
+            if bag.is_valid():
+                valid_bags.append(dirname)
+        print(valid_bags)
+##I want to output the valid bags list for use in the rsync.
