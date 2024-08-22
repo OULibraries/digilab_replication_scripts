7e42686047521647a1896f940eafb27bfb11fbd7
MP <mpmalahy@ou.edu>
2024-08-22 15:38:29 -0500

    added new file and updated current script

--- validate_and_sync_bags.py
+++ validate_and_sync_bags.py
@@ -8,10 +8,10 @@ import logging
 import sys
 import glob
 
-#from secrets.py import ACCESS_KEY_ID,SECRET_ACCESS_KEY,REGION,BUCKET_NAME 
+
 from botocore.exceptions import ClientError
 #variables imported from config.py must be listed explicitly
-#from config.py import 
+
 
 #substitute variable bag_path for 'path to object' in functions
 #use isdir in __main__
@@ -21,54 +21,45 @@ def main():
     bucket = sys.argv[3]
     s3_client = boto3.client('s3')
 
-    if not os.path.isdir(bag_path):
-        print("%s is not a directory." % bag_path)
-        return
+#    if not os.path.isdir(bag_path):
+#        print("%s is not a directory." % bag_path)
+#        return
     
  #   if not os.path.ismount(rsync_dest):
  #       print("%s is not a mounted share" % rsync_dest)
  #       return
     
-    try:
-        bag = bagit.Bag(bag_path)    
-        if not bag.is_valid(): 
-            print("%s is not a valid bag." % bag_path)
-            return
-    except:
-        print("not a bag")  
+#    try:
+#        bag = bagit.Bag(bag_path)    
+#        if not bag.is_valid(): 
+#            print("%s is not a valid bag." % bag_path)
+#            return
+#    except Exception as err:
+#        print(f"Unexpected {err=}, {type(err)=}")
+#        raise
 
 
 #if the bag is valid, pass the path to rsync
-
-#keep -v for testing but don't need in prod
-          
+       
    
-    subprocess.call(['rsync', '-av', '--dry-run', '--update', '--no-perms', '--omit-dir-times',"{0}".format(bag_path),"{0}".format(rsync_dest)])
+ #   subprocess.call(['rsync', '-av', '--dry-run', '--update', '--no-perms', '--omit-dir-times',"{0}".format(bag_path),"{0}".format(rsync_dest)])
+
 
     #iterate through bag and list files
-    files = glob.glob(bag_path)
-    files
-    #upload contents of the bag
-    for file in files:
-        upload_to_s3(file, 'ul-bagit')
-        print('uploaded ', file)
-
-
-
-#There is no '--dryrun' arg in boto3
-def upload_to_s3(file_name, bucket, object_name=None):
-    if object_name is None:
-        object_name = file_name
-
+#    files = glob.glob(bag_path)
+#    for file in files:
+#        try:
+#            response = s3_client.upload_file(file, Bucket='ul-bagit-test', Key=bag_path+file)
+#            print('uploaded ', file)
+#        except Exception as err:
+#            print(f"Unexpected {err=}, {type(err)=}")
+#            raise
     try:
-        response = s3_client.upload_to_s3(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-    print(response)
-
-
-
+        response = s3_client.put_object(Body=bag_path, Bucket='ul-bagit-test', Key=bag_path)
+        print('uploaded ', bag_path)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
+     
 if __name__ == "__main__":
     main()
