73e45db2d801735e798bbfa23cbf32fbdb53c762
MP <mpmalahy@ou.edu>
2024-08-26 14:51:14 -0500

    updated main wrapper script

--- filesync_workflow.py
+++ filesync_workflow.py
@@ -12,39 +12,38 @@ def main(bag_path, bag_name, bucket, s3_dest, tag, rsync_dest):
         print("%s is not a directory." % bag_path)
         return
         
-#copied from OULibq replicate_bag.py and modified
 
-#def _find_bag(bag_name):
-    """ 
-    Function returns path to nas and norfile on current worker. Determines if bag_name is on Nas1 or Nas2.
-    returns NAS Path , Norfile Path, S3 Bucket,S3 Key, S3 folder
-    """
-            
-    if os.path.isdir(os.path.join(bag_path, bag_name)):
-        nas_path = bag_path["nas"]["bagit"]
-    elif os.path.isdir(os.path.join(bag_path,bag_name)):
-        nas_path = bag_path["nas"]["bagit2"]
+
+def locate_bag(bag_path):
+           
+    if os.path.isdir(bag_path):
+        base_path = os.path.split(bag_path)[0]
+        bag_name = os.path.split(bag_path)[1]
+        s3_dest = os.path.split(base_path)[1]
     else:
-        raise Exception("Checked both NAS location and unable to find bag_name:{0}.".format(bag_name))
-    if "private" in bag_name or "preservation" in bag_name or "shareok" in bag_name:
-        s3_dest_dir="private"
+        raise Exception("{0} is not a valid bag path".format(bag_path))
+    if "private" in base_path or "preservation" in base_path or "shareok" in base_path:
+        s3_subdir = os.path.split(base_path)[1]
+        s3_base_dir="private"
+        s3_private_path = os.path.join(s3_base_dir,s3_subdir,bag_name)
+        return os.path.join(s3_private_path,bag_name)
     else:
-        s3_dest_dir="source"
-    return nas_path, bag_path["norfile"]["bagit"],bag_path["s3"]["bucket"],os.path.join(s3_dest_dir,bag_name),s3_dest_dir
+        s3_base_dir="source"
+        return os.path.join(s3_base_dir,bag_name),s3_base_dir
 
      #   source_dir = os.path.split(bag_path("/"))
          #   find path up to Bagit/ and take dir after "/"
         
      #   dest_dir = os.path.join(rsync_dest, source_dir)
 
-     #   s3_dest_dir = os.path.joint(bucket, source_dir)
+     #   s3_base_dir = os.path.joint(bucket, source_dir)
 
 # Now reassign bag_path, rsync_dest, and s3_dest with targeted directory values
     bag_path = source_dir
 
     rsync_dest = dest_dir
 
-    s3_dest = s3_dest_dir
+    s3_dest = s3_base_dir
 
     validate_rsync_and_upload_to_s3(bag_path, bucket, s3_dest, tag, rsync_dest)
         
