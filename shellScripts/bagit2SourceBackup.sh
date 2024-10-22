f7fa33719bcec8c9213de9d46a1e12896fcb1f4b
MP <mpmalahy@ou.edu>
2024-10-22 12:01:06 -0500

    added new script for different path handling in directories and updated shell script

--- shellScripts/bagit2SourceBackup.sh
+++ shellScripts/bagit2SourceBackup.sh
@@ -4,4 +4,4 @@
    and upload to S3 each bag given a source path on a NAS
 '''
 
-python findAndUploadBags.py --sourcePath /mnt/NAS2/Bagit2/ --bucket ul-bagit --syncDest /mnt/norfile/UL-BAGIT/source/ &> "source_$(date +%Y-%m-%d.txt)" 
+python findAndUploadBagsSource.py --sourcePath /mnt/NAS2/Bagit2/ --bucket ul-bagit --syncDest /mnt/norfile/UL-BAGIT/source/ &> "source_$(date +%Y-%m-%d.txt)" 
